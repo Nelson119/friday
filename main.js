c99c4b3536ddbe3d691e452df38c10869e4e9772
nelson119 <morphlina@gmail.com>
2015-07-21 22:03:59 +0800

    fixed parallax

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 $( document ).ready(function() {
   function parallax(){
-    $.each($('.layer1'), function(index, layer) {
-      layer.style.transform = 'translateY(' + -(window.pageYOffset*0.3)+'px)';
-      layer.style['-webkit-transform'] = 'translateY(' + -(window.pageYOffset*0.3)+'px)';
+    // $.each($('.layer1'), function(index, layer) {
+    //   layer.style.transform = 'translateY(' + -(window.pageYOffset*0.3)+'px)';
+    //   layer.style['-webkit-transform'] = 'translateY(' + -(window.pageYOffset*0.3)+'px)';
+    // });
+    TweenMax.set($('.layer1'),{
+      marginTop : $(window).scrollTop() * -0.3
     });
-
-    $.each($('.layer2'), function(index, layer) {
-      console.log(layer.outerHTML);
-      layer.style.transform = 'translateY(' + -(window.pageYOffset*0.5)+'px)';
-      layer.style['-webkit-transform'] = 'translateY(' + -(window.pageYOffset*0.5)+'px)';
+    TweenMax.set($('.layer2'),{
+      marginTop : $(window).scrollTop() * -0.5
     });
+    // TweenMax.set( $('div.page'),{
+    //   marginTop : $(window).scrollTop() * 0.5
+    // });
+    // $.each($('.layer2'), function(index, layer) {
+    //   console.log(layer.outerHTML);
+    //   layer.style.transform = 'translateY(' + -(window.pageYOffset*0.5)+'px)';
+    //   layer.style['-webkit-transform'] = 'translateY(' + -(window.pageYOffset*0.5)+'px)';
+    // });
 
     $('div.page').each(function(){
         if (
@@ -30,6 +38,6 @@ $( document ).ready(function() {
 
   }
 
-  window.addEventListener('scroll', parallax, false);
+  $(window).on('scroll', parallax);//, false);
   $('a').smoothScroll({speed: 1200});
 });
